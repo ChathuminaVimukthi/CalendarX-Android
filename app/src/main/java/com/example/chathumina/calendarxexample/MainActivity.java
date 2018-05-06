26f99b0de7d79550f06f095f37757cc5a97c42a6
Chathumina Vimukthi <chathumina@Chathuminas-MacBook-Air.local>
2018-05-06 16:21:55 +0530

    Demo CalendarView layout created

--- app/src/main/java/com/example/chathumina/calendarxexample/MainActivity.java
+++ app/src/main/java/com/example/chathumina/calendarxexample/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.chathumina.calendarxexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.calendar_x.CalendarDebug;
+import com.example.calendar_x_main.CalendarX;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalendarX cal = findViewById(R.id.chathumina_Cal);
+        cal.setUserCurrentMonthYear(2, 2017);
+
+        cal.setCallBack(new CalendarX.DayClickListener() {
+            @Override
+            public void onDayClick(View view) {
+
+            }
+        });
     }
 }
