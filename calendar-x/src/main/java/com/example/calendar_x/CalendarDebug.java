8903d0eb829cb9f89ad0cab61bdabe7149a6f3c6
Chathumina Vimukthi <chathumina@Chathuminas-MacBook-Air.local>
2018-05-06 15:12:44 +0530

    Created project in A-Studio. Created CalendarDebug for easy debuggin

---
+++ calendar-x/src/main/java/com/example/calendar_x/CalendarDebug.java
@@ -0,0 +1,12 @@
+package com.example.calendar_x;
+
+import android.util.Log;
+
+public class CalendarDebug {
+
+    private static final String APP_NAME = "CALENDAR X";
+
+    public static void log(String message){
+        Log.d(APP_NAME,message);
+    }
+}
